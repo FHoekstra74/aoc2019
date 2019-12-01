4b6681f284b15b20db266a73c86cd4944e8331a5
frans hoekstra <frans.hoekstra@gmail.com>
2019-12-01 09:12:07 +0100

    day1

---
+++ aoc2019/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace aoc2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watch = Stopwatch.StartNew();
+            day1.Go();
+            watch.Stop();
+
+            Console.WriteLine("Time taken: {0} ms ({1} minutes)", watch.ElapsedMilliseconds, watch.Elapsed.Minutes);
+            //Console.ReadKey();
+        }
+    }
+}
---
+++ aoc2019/day1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+namespace aoc2019
+{
+    public class day1
+    {
+        public static void Go()
+        {
+            var values = File.ReadAllLines(@"c:\github\aoc2019\input\day1.txt").Select(line => int.Parse(line));
+            int resultA = 0;
+            int resultB = 0;
+            foreach (int i in values)
+            {
+                resultA += Fuel(i);
+                resultB += TotalFuel(i);
+            }
+            Console.WriteLine(resultA.ToString());
+            Console.WriteLine(resultB.ToString());
+        }
+
+        private static int TotalFuel(int mass)
+        {
+            int total = 0;
+            int val = Fuel(mass);
+            while (val>0)
+            {
+                total += val;
+                val = Fuel(val);
+            }
+            return total;
+        }
+        private static int Fuel(int mass)
+        {
+            return mass / 3 - 2;
+        }
+    }
+}
