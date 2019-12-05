eda0259871207dcf98a502f29070549170fa3645
frans hoekstra <frans.hoekstra@gmail.com>
2019-12-05 19:02:16 +0100

    some day5 c# tweaks, could be handy for dayN

--- aoc2019/day5.cs
+++ aoc2019/day5.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.IO;
 using System.Linq;
+using System.Collections;
 
 namespace aoc2019
 {
@@ -13,10 +13,12 @@ namespace aoc2019
             string input = File.ReadLines(@"c:\github\aoc2019\input\day5.txt").First();
             List<int> items = input.Split(',').Select(Int32.Parse).ToList();
             Console.WriteLine("PartA:");
-            run(items, 1);
+            Stack inputs = new Stack();
+            inputs.Push(1);
+            run(new List<int>(items), inputs);
+            inputs.Push(5);
             Console.WriteLine("PartB:");
-            items = input.Split(',').Select(Int32.Parse).ToList();
-            run(items, 5);
+            run(items, inputs);
         }
 
         private static int getparam(List<int> items, int paramnum, int pointer, bool position)
@@ -27,52 +29,37 @@ namespace aoc2019
                 return items[pointer + paramnum];
         }
 
-        private static int run(List<int> items, int machineocode)
+        private static int run(List<int> items, Stack inputs)
         {
-            int pointer = 0;
-            List<int> param1needed = new List<int> { 1, 2, 4, 5, 6, 7, 8 };
-            List<int> param2needed = new List<int> { 1, 2, 5, 6, 7, 8 };
-
+            int pointer, param1, param2, steps;
+            pointer = param1 = param2 = steps = 0;
             while (true)
             {
-                string opcodes = items[pointer].ToString();
-
-                while (opcodes.Length < 5)
-                    opcodes = "0" + opcodes;
-
-                int mode1 = int.Parse(opcodes.Substring(2, 1));
-                int mode2 = int.Parse(opcodes.Substring(1, 1));
-                int mode3 = int.Parse(opcodes.Substring(0, 1));
+                steps += 1;
+                string opcodes = items[pointer].ToString().PadLeft(5,'0');
                 int opcode = int.Parse(opcodes.Substring(3, 2));
 
-                int param1=0;
-                int param2=0;
-
-                if (param1needed.Contains(opcode))
-                    param1 = getparam(items, 1, pointer, mode1 == 0);
-                if (param2needed.Contains(opcode))
-                    param2 = getparam(items, 2, pointer, mode2 == 0);
+                if (new[] { 1, 2, 4, 5, 6, 7, 8 }.Contains(opcode))
+                    param1 = getparam(items, 1, pointer, (opcodes[2] - 48) == 0);
+                if (new[] { 1, 2, 5, 6, 7, 8 }.Contains(opcode))
+                    param2 = getparam(items, 2, pointer, (opcodes[1] - 48) == 0);
 
                 switch (opcode)
                 {
                     case 99:
-                        Console.WriteLine("STOP");
+                        Console.WriteLine(string.Format("FINISHED IN {0} STEPS",steps));
                         return 0;
                     case 1:
                         items[items[pointer + 3]] = param1 + param2;
-                        pointer += 4;
                         break;
                     case 2:
                         items[items[pointer + 3]] = param1 * param2;
-                        pointer += 4;
                         break;
                     case 3:
-                        items[items[pointer + 1]] = machineocode;
-                        pointer += 2;
+                        items[items[pointer + 1]] = Convert.ToInt32(inputs.Pop());
                         break;
                     case 4:
                         Console.WriteLine(param1);
-                        pointer += 2;
                         break;
                     case 5:
                         if (param1 != 0)
@@ -91,18 +78,20 @@ namespace aoc2019
                             items[items[pointer + 3]] = 1;
                         else
                             items[items[pointer + 3]] = 0;
-                        pointer += 4;
                         break;
                     case 8:
                         if (param1 == param2)
                             items[items[pointer + 3]] = 1;
                         else
                             items[items[pointer + 3]] = 0;
-                        pointer += 4;
                         break;
                     default:
                         break;
                 }
+                if (new[] { 1, 2, 7, 8 }.Contains(opcode))
+                    pointer += 4;
+                if (new[] { 3, 4 }.Contains(opcode))
+                    pointer += 2;
             }
         }
     }
