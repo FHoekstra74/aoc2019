7be2abb1d215bb5a0a03c01ea3c5c64994361d5f
frans hoekstra <frans.hoekstra@gmail.com>
2019-12-08 20:42:05 +0100

    day7 in 40 lines of python

--- python/day7.py
+++ python/day7.py
@@ -1,12 +1,8 @@
 import queue 
 def valid(nr,validvals):
-    t=[]
     for c in nr:
-        if int(c) in validvals:
-            if c in t: return(False) 
-            else: t.append(c)
-        else: return(False)
-    return (True)
+        if int(c) not in validvals: return (False)
+    return len(set(nr)) == len(nr)
 def run(nums,input,output,pointer):
     opcode=0
     while opcode < 99:
@@ -28,24 +24,17 @@ def run(nums,input,output,pointer):
         if opcode in [1,2,7,8]: pointer[0]+=4
         elif opcode in [3,4]: pointer[0]+=2
     return(-99999)
-nums = [int(n) for n in open("..\input\day7.txt", "r").read().split(',')]
-highest = [0] * 2
+nums,highest = [int(n) for n in open("..\input\day7.txt", "r").read().split(',')], [0] * 2
 for j in range(0,100000):
-    s=f"{j:05}"
-    if valid(s,[0,1,2,3,4]) or valid(s,[5,6,7,8,9]):
-        q,check=[],0
+    if valid(f"{j:05}",[0,1,2,3,4]) or valid(f"{j:05}",[5,6,7,8,9]):
+        q,c,p,res=[],[],[],0
         for i in range (5): q.append(queue.Queue(maxsize=20))
-        for i in range (5): q[i].put(int(s[i]))
+        for i in range (5): q[i].put(int(f"{j:05}"[i]))
+        for i in range (5): c.append(nums.copy())
+        for i in range (5): p.append([0])
         q[0].put(0)
-        if valid(s,[0,1,2,3,4]):
-            for i in range (5): run(nums.copy(),q[i],q[i+1 if i<4 else 0],[0].copy())
-        else:
-            c,p,res,check=[],[],0,1
-            for i in range (5): c.append(nums.copy())
-            for i in range (5): p.append([0])
-            while res !=-99999: 
-                for i in range (5): res=run(c[i],q[i],q[i+1 if i<4 else 0],p[i])
+        while res !=-99999 or (j<5000 and res==0): 
+            for i in range (5): res=run(c[i],q[i],q[i+1 if i<4 else 0],p[i])
         res=q[0].get()
-        if res>highest[check]: highest[check]=res
-print("AnswerA: ",highest[0])
-print("AnswerB: ",highest[1])
+        if res>highest[1 if j>5000 else 0]: highest[1 if j>5000 else 0]=res
+print("AnswerA: ",highest[0],"\nAnswerB: ",highest[1])
