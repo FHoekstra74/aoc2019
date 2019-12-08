d397a6bb33aec7540f7bc79aa392c96680b9a3f0
frans hoekstra <frans.hoekstra@gmail.com>
2019-12-08 19:02:14 +0100

    day 7 in python

---
+++ python/day7.py
@@ -0,0 +1,61 @@
+import queue 
+
+def valid(nr,validvals):
+    t=[]
+    for c in nr:
+        if int(c) in validvals:
+            if c in t: return(False) 
+            else: t.append(c)
+        else: return(False)
+    return (True)
+
+def run(nums,input,output,pointer):
+    opcode=0
+    while opcode < 99:
+        t=f"{nums[pointer[0]]:05}"
+        opcode,b,c=int(t[3:]),int(t[1]),int(t[2])
+        if opcode != 3 and opcode != 99: par1 = nums[nums[pointer[0] + 1]] if c == 0 else nums[pointer[0] + 1]
+        if opcode != 4 and opcode != 3 and opcode != 99: par2 = nums[nums[pointer[0] + 2]] if b == 0 else nums[pointer[0] + 2]
+        if opcode == 1: nums[nums[pointer[0] + 3]] = par1 + par2
+        elif opcode == 2: nums[nums[pointer[0] + 3]] = par1 * par2
+        elif opcode == 3: nums[nums[pointer[0] + 1]] = input.get()
+        elif opcode == 4: 
+            output.put(par1)
+            pointer[0]+=2
+            return(par1)
+        elif opcode == 5: pointer[0]=par2 if par1 != 0 else pointer[0]+3
+        elif opcode == 6: pointer[0]=par2 if par1 == 0 else pointer[0]+3
+        elif opcode == 7: nums[nums[pointer[0] + 3]] = 1 if par1 < par2 else 0
+        elif opcode == 8: nums[nums[pointer[0] + 3]] = 1 if par1 == par2 else 0
+        if opcode in [1,2,7,8]: pointer[0]+=4
+        elif opcode in [3,4]: pointer[0]+=2
+    return(-99999)
+
+nums = [int(n) for n in open("..\input\day7.txt", "r").read().split(',')]
+highestA,highestB=0,0
+for j in range(0,100000):
+    s=f"{j:05}"
+    if valid(s,[0,1,2,3,4]):
+        q=[]
+        for i in range (5): q.append(queue.Queue(maxsize=20))
+        for i in range (5): q[i].put(int(s[i]))
+        q[0].put(0)
+        for i in range (4): run(nums.copy(),q[i],q[i+1],[0].copy())
+        run(nums.copy(),q[4],q[0],[0])
+        res=(q[0].get())
+        if res>highestA: highestA=res
+    elif valid(s,[5,6,7,8,9]):
+        q,c,p,res=[],[],[],0
+        for i in range (5): 
+            q.append(queue.Queue(maxsize=20))
+            c.append(nums.copy())
+            p.append([0])
+        for i in range (5): q[i].put(int(s[i]))
+        q[0].put(0)
+        while res !=-99999:
+            for i in range (4): run(c[i],q[i],q[i+1],p[i])
+            res = run(c[4],q[4],q[0],p[4])
+        res=q[0].get()
+        if res>highestB: highestB=res
+print("AnswerA: ",highestA)
+print("AnswerB: ",highestB)
