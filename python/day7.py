761ef9253290ca31c6d323fb04b655642ebc2b01
frans hoekstra <frans.hoekstra@gmail.com>
2019-12-08 19:39:08 +0100

    some day7 python tweaks

--- python/day7.py
+++ python/day7.py
@@ -1,5 +1,4 @@
 import queue 
-
 def valid(nr,validvals):
     t=[]
     for c in nr:
@@ -8,7 +7,6 @@ def valid(nr,validvals):
             else: t.append(c)
         else: return(False)
     return (True)
-
 def run(nums,input,output,pointer):
     opcode=0
     while opcode < 99:
@@ -30,32 +28,24 @@ def run(nums,input,output,pointer):
         if opcode in [1,2,7,8]: pointer[0]+=4
         elif opcode in [3,4]: pointer[0]+=2
     return(-99999)
-
 nums = [int(n) for n in open("..\input\day7.txt", "r").read().split(',')]
-highestA,highestB=0,0
+highest = [0] * 2
 for j in range(0,100000):
     s=f"{j:05}"
-    if valid(s,[0,1,2,3,4]):
-        q=[]
+    if valid(s,[0,1,2,3,4]) or valid(s,[5,6,7,8,9]):
+        q,check=[],0
         for i in range (5): q.append(queue.Queue(maxsize=20))
         for i in range (5): q[i].put(int(s[i]))
         q[0].put(0)
-        for i in range (4): run(nums.copy(),q[i],q[i+1],[0].copy())
-        run(nums.copy(),q[4],q[0],[0])
-        res=(q[0].get())
-        if res>highestA: highestA=res
-    elif valid(s,[5,6,7,8,9]):
-        q,c,p,res=[],[],[],0
-        for i in range (5): 
-            q.append(queue.Queue(maxsize=20))
-            c.append(nums.copy())
-            p.append([0])
-        for i in range (5): q[i].put(int(s[i]))
-        q[0].put(0)
-        while res !=-99999:
-            for i in range (4): run(c[i],q[i],q[i+1],p[i])
-            res = run(c[4],q[4],q[0],p[4])
+        if valid(s,[0,1,2,3,4]):
+            for i in range (5): run(nums.copy(),q[i],q[i+1 if i<4 else 0],[0].copy())
+        else:
+            c,p,res,check=[],[],0,1
+            for i in range (5): c.append(nums.copy())
+            for i in range (5): p.append([0])
+            while res !=-99999: 
+                for i in range (5): res=run(c[i],q[i],q[i+1 if i<4 else 0],p[i])
         res=q[0].get()
-        if res>highestB: highestB=res
-print("AnswerA: ",highestA)
-print("AnswerB: ",highestB)
+        if res>highest[check]: highest[check]=res
+print("AnswerA: ",highest[0])
+print("AnswerB: ",highest[1])
